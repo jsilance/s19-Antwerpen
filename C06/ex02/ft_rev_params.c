09945ff56427c74ae012b1f522c990bf53b54a52
juliensilance@hotmail.com <juliensilance@mbp-de-julien.home>
2023-03-01 23:44:04 +0100

    finishing c06

--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_rev_params.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jusilanc <jusilanc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: juliensilance <juliensilance@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 19:35:20 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/03/01 19:41:28 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/03/01 20:25:34 by juliensilan      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,10 @@ int	ft_strlen(char *str)
 
 int	main(int argc, char **argv)
 {
-	while (1 <= --argc)
+	while (1 < argc--)
 	{
 		write(1, argv[argc], ft_strlen(argv[argc]));
-		if (1 < argc)
-			write(1, "\n", 1);
+		write(1, "\n", 1);
 	}
 	return (0);
 }
--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juliensilance <juliensilance@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/01 16:25:58 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/03/01 20:22:20 by juliensilan      ###   ########.fr       */
+/*   Created: 2023/03/01 21:42:45 by juliensilan       #+#    #+#             */
+/*   Updated: 2023/03/01 23:32:56 by juliensilan      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,60 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
+void	ft_swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	if (!s1)
+		return (1);
+	if (!s2)
+		return (-1);
+	while (s1 && s2)
+	{
+		if (!*s1 || !*s2 || (*(s1) != *(s2)))
+		{
+			if (*s1 > *s2)
+				return (*s1 - *s2);
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_sort_char_tab(char **tab, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (ft_strcmp(tab[i], tab[j]) > 0)
+				ft_swap(&tab[i], &tab[j]);
+			j++;
+		}
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int	i;
 
 	i = 1;
+	ft_sort_char_tab(argv, argc);
 	while (i < argc)
 	{
 		write(1, argv[i], ft_strlen(argv[i]));
