dabfa4d6c2d2eaef85cc05fbfbc7f3641276f172
juliensilance@hotmail.com <juliensilance@MacBook-Pro-de-Julien.local>
2023-03-03 07:36:11 +0100

    finishin C07++

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -1,64 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*   ft_split2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juliensilance <juliensilance@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/02 14:26:42 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/03/02 23:13:22 by juliensilan      ###   ########.fr       */
+/*   Created: 2023/03/03 06:29:58 by juliensilan       #+#    #+#             */
+/*   Updated: 2023/03/03 07:33:40 by juliensilan      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-
-char	*ft_strstr(char *str, char *to_find)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	if (!to_find || !*to_find)
-		return (str);
-	while (str && str[i])
-	{
-		j = 0;
-		while (str[i + j] && str[i + j] == to_find[j])
-			if (!to_find[++j])
-				return (&str[i]);
-		i++;
-	}
-	return ((void *)0);
-}
 
 int	ft_strchr(char *str, char c)
 {
 	int		i;
 
 	i = 0;
-	while (str && str[i])
+	if (!c)
+		return (0);
+	while (str && str[i] && c)
 	{
 		if (str[i] == c)
 			return (i);
 		i++;
 	}
 	return (-1);
-}
-
-char	*ft_strndup(char *str, int nb)
-{
-	char	*newstr;
-	int		i;
-
-	if (str)
-		newstr = malloc(sizeof(char) * (nb + 1));
-	if (!newstr)
-		return ((void *)0);
-	i = -1;
-	while (str && str[++i] && nb-- > 0)
-		newstr[i] = str[i];
-	newstr[i] = '\0';
-	return (newstr);
 }
 
 int	ft_word_counter(char *str, char *charset)
@@ -80,45 +47,43 @@ int	ft_word_counter(char *str, char *charset)
 	return (word);
 }
 
-char	*ft_start_word(char *str, char *charset)
+char	*ft_strndup(char *str, int start, int end)
 {
-	int	i;
+	char	*ptr;
+	int		i;
 
 	i = 0;
-	while (str && *str)
-	{
-		if (ft_strchr(charset, *str) > -1)
-			str++;
-		else
-		{
-			while (ft_strchr(charset, str[i]) == -1)
-				i++;
-			return (ft_strndup(str, i)); // return un mot mais faut trouver le point de depart pour la suite
-		}
-	}
+	ptr = malloc(sizeof(char) * end - start + 1);
+	if (!ptr)
+		return ((void *)0);
+	while (start < end)
+		ptr[i++] = str[start++];
+	ptr[i] = '\0';
+	return (ptr);
 }
 
-#include <stdio.h>
 char	**ft_split(char *str, char *charset)
 {
 	char	**ptr;
 	int		i;
+	int		j;
+	int		word_count;
 
 	i = 0;
-	ptr = (char **)malloc(sizeof(char*) * (ft_word_counter(str, charset) + 1));
+	word_count = ft_word_counter(str, charset);
+	ptr = (char **)malloc(sizeof(char*) * (word_count + 1));
 	if (!ptr)
 		return ((void *)0);
-	while (str && *str)
+	while (str && *str && i < word_count)
 	{
-		ptr[i] = ft_start_word(str, charset);
-		str = ft_strstr(str, ptr[i++]);
-		ptr[i] = (void *)0;
+		while (str && *str && ft_strchr(charset, *str) > -1)
+			str++;
+		j = 0;
+		while (str && *str && ft_strchr(charset, str[j]) == -1)
+			j++;
+		ptr[i++] = ft_strndup(str, 0, j);
+		str += j;
 	}
+	ptr[i] = 0;
 	return (ptr);
-}
-
-
-int main(void)
-{
-	ft_split("yolo les amis +on-*+fait la teuf?", " +-*");
 }
