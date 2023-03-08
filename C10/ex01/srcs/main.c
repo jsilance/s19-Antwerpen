77df245af0c842764e9b62436b94379af85fb154
juliensilance@hotmail.com <juliensilance@MacBook-Pro-de-Julien.local>
2023-03-08 11:51:49 +0100

    C10 ex01

---
+++ C10/ex01/srcs/main.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jusilanc <jusilanc@s19.be>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 10:32:17 by jusilanc          #+#    #+#             */
+/*   Updated: 2023/03/08 11:49:59 by jusilanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
+}
+
+int	ft_error(int argc)
+{
+	if (argc < 0)
+		write(2, "Cannot read file.\n", 18);
+	else if (argc == 1)
+		write(2, "File name missing.\n", 19);
+	else if (argc > 2)
+		write(2, "Too many arguments.\n", 20);
+	return (1);
+}
+
+int main(int argc, char **argv)
+{
+	int		fd;
+	char	buffer[32];
+	int		i;
+	int		eof;
+
+	fd = -1;
+	eof = 31;
+	i = 0;
+	if (argc < 2)
+		return (ft_error(argc));
+	while (++i < argc)
+	{
+		fd = open(argv[i], O_RDONLY);
+		if (fd < 0)
+			return (ft_error(fd));
+		while (eof > 0 && eof == 31)
+		{
+			eof = read(fd, buffer, 31);
+			buffer[eof] = '\0';
+			write(1, buffer, ft_strlen(buffer));
+		}
+		if (eof < 0)
+			return (ft_error(eof));
+		close(fd);
+	}
+	return (0);
+}
