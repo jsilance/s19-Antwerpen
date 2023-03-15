794777990792be58bb28c8b9b9d89be86d8f9380
juliensilance@hotmail.com <juliensilance@MacBook-Pro-de-Julien.local>
2023-03-15 20:53:27 +0100

    correction C10

--- C10/ex00/srcs/main.c
+++ C10/ex00/srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jusilanc <jusilanc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jusilanc <jusilanc@s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 10:32:17 by jusilanc          #+#    #+#             */
-/*   Updated: 2023/03/15 13:26:18 by jusilanc         ###   ########.fr       */
+/*   Updated: 2023/03/15 20:49:52 by jusilanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,16 @@ int	ft_error(int argc)
 int	main(int argc, char **argv)
 {
 	int		fd;
-	char	buffer[32];
-	int		eof;
+	char	buffer;
 
 	fd = -1;
-	eof = 31;
 	if (argc != 2)
 		return (ft_error(argc));
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 		return (ft_error(fd));
-	while (read(fd, buffer, 32))
-		write(1, buffer, 32);
+	while (read(fd, &buffer, 1))
+		write(1, &buffer, 1);
 	write(1, "\n", 1);
 	close(fd);
 	return (0);
